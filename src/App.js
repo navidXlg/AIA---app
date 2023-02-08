af785a28a20ce04b1bff12398be6897093fad8bd
NavidKhaleghi <navidjoker669@gamil.com>
2023-02-08 23:15:02 +0330

    project has been finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-
+import "bulma/css/bulma.css";
 import PostCard from "./PostCard";
+import Alexa from "./imges/Alexa.png";
+import Siri from "./imges/Appel.png";
+import Cortona from "./imges/Cortona.png";
 
 function App(){
 
     return(
         <>
-            <div>Personal digital Asistant</div>
-
-
-            <PostCard/>
-            <PostCard/>
-            <PostCard/>
+        <section class="hero is-small is-primary">
+            <div class="hero-body">
+                <p class="title">
+                   Digital Asistent Programm
+                </p>
+            </div>
+        </section>
+        <div className="container">
+            <div className="section">
+            <div className="columns">
+                <div className="column is-3 is-offset-1 "><PostCard titel = "Alexa" imgSrc = {Alexa} heading = "@Alexa99"/></div>
+                <div className="column is-3 is-offset-1 "><PostCard titel = "Cortana" imgSrc = {Siri} heading = "@Cortan32"/></div>
+                <div className="column is-3 is-offset-1 "><PostCard titel = "Siri"  imgSrc = {Cortona} heading = "@Siri"/></div>
+            </div>
+            </div>
+        </div>
         </>
+
+
+
     );
 };
 
--- src/PostCard.js
+++ src/PostCard.js
@@ -1,14 +1,21 @@
 
-function PostCard(){
-
+function PostCard({titel, heading ,imgSrc}){
 
     return(
 
-        <div></div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-4by3">
+                    <img src = {imgSrc} alt = "logo-pic"/>
+                </figure>
+            </div>
+            <div className="card-content">
+                <p className="title is-4">{titel}</p>
+                <p className="subtitle is-4">{heading}</p>
+            </div>
+        </div>
     );
 
-
 };
-
 
 export default PostCard;
